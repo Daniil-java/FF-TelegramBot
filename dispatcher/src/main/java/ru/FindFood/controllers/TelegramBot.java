a6c4854292ae01107ef20185969fb1495b4f8d6f
ПК <saponembigas@gmail.com>
2023-07-26 17:30:08 +0800

    TS10: Init

---
+++ dispatcher/src/main/java/ru/FindFood/controllers/TelegramBot.java
@@ -0,0 +1,52 @@
+package ru.FindFood.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.FindFood.configs.BotConfig;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot {
+    private final BotConfig botConfig;
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update);
+//        var originalMessage = update.getMessage();
+//        log.debug(originalMessage.getText());
+//
+//        var response = new SendMessage();
+//        response.setChatId(originalMessage.getChatId().toString());
+//        response.setChatId("Hello");
+//        sendAnswerMessage(response);
+
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+}
